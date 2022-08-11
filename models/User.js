82802068f51e075c3f8b687f40ec69704e4afb16
Cole Simpson <cole22simpson@gmail.com>
2022-08-11 15:16:41 -0700

    minor tweak to user.js

--- models/User.js
+++ models/User.js
@@ -1,16 +1,16 @@
 const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-
+const userSchema = new Schema(
+  {
     username: {
         type: String,
         unique: true,
-        required: true,
+        required: 'You must create a username.',
         trim: true
     },
     email: {
         type: String,
-        required: true,
+        required: 'You must add a valid email.',
         unique: true,
         match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
     },
@@ -34,8 +34,8 @@ const userSchema = new Schema({
   }
 );
 
-  userSchema.virtual('friendCount').get(function() {
-      return this.friends.length;
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 });
 
 const User = model('User', userSchema);
